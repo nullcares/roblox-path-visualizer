96010797711d4de84ed6304e7451ec8ab4b47814
nullcares <89300340+nullcares@users.noreply.github.com>
2022-07-31 16:50:46 +0200

    Update example.lua

--- example.lua
+++ example.lua
@@ -7,14 +7,14 @@ local hum = char:FindFirstChild('Humanoid')
 local path = pfs:CreatePath()
 path:ComputeAsync(Vector3.new(), Vector3.new(50, 0, 50))
 if path.Status == Enum.PathStatus.Success then
-  local waypoints = path:GetWaypoints()
-  local visualizedpath = visualizer:VisualizePath(waypoints)
-  for _, waypoint in pairs(waypoints) do
-    hum:MoveTo(waypoint.Position)
-    if waypoint.Action == Enum.PathWaypointAction.Jump then
-      hum.Jump = true
-    end
-    hum.MoveToFinished:Wait(0)
-  end
-  visualizedpath:RemovePath()
+	local waypoints = path:GetWaypoints()
+	local visualizedpath = visualizer:VisualizePath(waypoints)
+	for _, waypoint in pairs(waypoints) do
+		hum:MoveTo(waypoint.Position)
+		if waypoint.Action == Enum.PathWaypointAction.Jump then
+			hum.Jump = true
+		end
+		hum.MoveToFinished:Wait(0)
+	end
+	visualizedpath:RemovePath()
 end
